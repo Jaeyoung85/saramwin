ad55a7eb7c63d7c5860d4db8ac8838d26db91943
Jaeyoung85 <46377460+Jaeyoung85@users.noreply.github.com>
2019-01-06 15:58:17 +0900

    email and recapcha test

---
+++ js/vendor/email.js
@@ -0,0 +1,28 @@
+/**
+ * 이메일 보내기 
+ */
+
+function sendEmailCheck() {
+	
+	var from_nameStr = document.emailForm.from_name.value;
+	var from_messageStr = document.emailForm.from_message.value;
+	var reply_emailStr = document.emailForm.reply_email.value;
+	
+	var service_id = 'gmail';
+	var template_id = 'template_vmaqxyqf';
+	var template_params = {
+		from_name: from_nameStr,
+		reply_email: reply_emailStr,
+		from_message: from_messageStr
+	};
+	if(from_nameStr != null){
+		if(from_messageStr != null){
+			if(reply_emailStr != null){
+				/*emailjs.send(service_id,template_id,template_params);*/
+				document.getElementById('emailModal').style.display='block';
+				var frm = document.getElementsByName('emailForm')[0];
+				frm.reset();
+			}
+		}
+	}
+}
