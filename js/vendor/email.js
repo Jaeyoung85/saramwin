e83b0436e1e9dd1af1555ab69b360a47c00f7fd3
Jaeyoung85 <46377460+Jaeyoung85@users.noreply.github.com>
2019-01-06 16:19:40 +0900

    recapcha test

--- js/vendor/email.js
+++ js/vendor/email.js
@@ -3,28 +3,24 @@
  */
 
 function sendEmailCheck() {
-	
+
 	var from_nameStr = document.emailForm.from_name.value;
 	var from_messageStr = document.emailForm.from_message.value;
 	var reply_emailStr = document.emailForm.reply_email.value;
-	
+	var result = grecaptcha.getResponse();
 	var service_id = 'gmail';
 	var template_id = 'template_vmaqxyqf';
 	var template_params = {
-		from_name: from_nameStr,
-		reply_email: reply_emailStr,
-		from_message: from_messageStr
+			from_name: from_nameStr,
+			reply_email: reply_emailStr,
+			from_message: from_messageStr
 	};
-	console.log(document.getElementById("html_element"));
-	console.log(grecaptcha.getResponse());
-	/*if(from_nameStr != null){
-		if(from_messageStr != null){
-			if(reply_emailStr != null){
-				emailjs.send(service_id,template_id,template_params);
-				document.getElementById('emailModal').style.display='block';
-				var frm = document.getElementsByName('emailForm')[0];
-				frm.reset();
-			}
-		}
-	}*/
+	if(result.length != 0){
+		/*emailjs.send(service_id,template_id,template_params);*/
+		document.getElementById('emailModal').style.display='block';
+		var frm = document.getElementsByName('emailForm')[0];
+		frm.reset();
+	}else{
+		document.getElementById('capchaModal').style.display='block';
+	}
 }
